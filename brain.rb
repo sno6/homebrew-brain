860db8e0a6100eeba6f169a0e0034238973a96d6
sno6 <farleyschaefer@gmail.com>
2022-10-25 21:28:58 +1300

    Update hashsums

--- brain.rb
+++ brain.rb
@@ -4,7 +4,6 @@
 class Brain < Formula
   desc "Beautiful notes in your terminal."
   homepage "https://github.com/sno6/brain"
-  sha256 "6140e07ae5dcf370e2dc1d41596edf100c2122b4ddc30567b79e35421abad7c6"
   license ""
 
   # depends_on "cmake" => :build
@@ -12,16 +11,20 @@ class Brain < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/sno6/brain/releases/download/v0.0.3/brain-arm64"
+      sha256 "530ae5ffb4edd0d1c5f231913e49e7b1bc6ef600bad960f6b54219a420251f62"
     else
       url "https://github.com/sno6/brain/releases/download/v0.0.3/brain-amd64"
+      sha256 "1c42f34f60f9a5059455e0c93f9e561b9894811d059af2acc3502afbc57ce045"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/sno6/brain/releases/download/v0.0.3/brain-linux-arm64"
+      sha256 "859473c83ce2d1f1398fe175cd88b2346a23e029dc04bf8877fb88c375813d47"
     else
       url "https://github.com/sno6/brain/releases/download/v0.0.3/brain-linux-amd64"
+      sha256 "f8b17be2a83987b33909982d6d816af479da52d1e3b302865fa7b799d2e6c8aa"
     end
   end
 
